d3d9cb28ba07fa68caf28c24da168f450b05d7c6
Ming Li <minglii@nexuscml00.umiacs.umd.edu>
2023-10-15 19:22:56 -0400

    init

--- scripts/reflect_instruction.sh
+++ scripts/reflect_instruction.sh
@@ -1,4 +1,4 @@
-python reflecn_instruction.py \
+python reflect_instruction.py \
     --data_path xxx.json \
     --save_path xxx_instruction.json \
     --api_key xxx 
---
+++ scripts/reflect_response_postprocess.sh
@@ -0,0 +1,6 @@
+python reflect_response_postprocess.py \
+    --raw_data_path xxx_response.json \
+    --ori_data_path xxx.json \
+    --save_path xxx_res_post.json \
+    --save_intermediate_path xxx_res_mid.json \
+    --api_key xxx 
